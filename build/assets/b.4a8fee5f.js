36a67c431f63cde66b0ccb6e26ac7af36584ed62
ustcwrn <wangruonan@mail.ustc.edu.cn>
2023-05-28 20:58:16 +0800

    feat: template-inject-dev

---
+++ build/assets/b.4a8fee5f.js
@@ -0,0 +1,1 @@
+import{a as r}from"./client-entry.21cf8eaa.js";function o(){return r("div",{children:"Hello, route B"})}export{o as default};
--- build/assets/b.980a14af.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"./client-entry.937eda20.js";function o(){return r("div",{children:"Hello, route B"})}export{o as default};
